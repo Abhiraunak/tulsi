4a0eb15596ac6702cc1a34996509f1b7e4ee891f
Abhishek <abhishekkp1721@hotmail.com>
2025-02-18 14:07:39 +0530

    Update the appbar component

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       {/* Main Content */}
       <main className="relative w-full h-[35rem]  bg-[linear-gradient(to_right,#f0f0f0_2px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_2px,transparent_1px)] bg-[size:6rem_4rem]">
         <Appbar />
-        <div className="container max-w-screen-xl mx-auto pt-[5rem] text-center space-y-6 sm:space-y-2">
+        <div className="container max-w-screen-xl mx-auto pt-12 text-center space-y-6 sm:space-y-2">
           <h1 className="text-4xl md:text-6xl lg:text-8xl font-bold lg:font-semibold md:font-semibold tracking-tight">
             From Concept to Reality, We Build Your Vision
           </h1>
@@ -43,7 +43,7 @@ export default function Home() {
           <div className="flex justify-center gap-6 lg:gap-14 pt-10 sm:pt-2">
             <Link href="/services">
               <button className="px-10 py-2 lg:min-w-[240px] bg-slate-600 hover:bg-slate-800 rounded-lg font-semibold text-white lg:text-xl text-center">
-                Service
+                Services
               </button>
             </Link>
 
--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,19 +1,36 @@
 "use client"
+import { Telescope, Zap } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Appbar() {
     return (
-        <div className='max-w-screen-xl mx-auto h-[80px] flex items-center justify-between px-4'>
-            <div className="nleft flex items-center">
-                <Link href={"/"} >
-                    <Image
-                        src="/tulsi-logo1.png"
-                        width={100}
-                        height={100}
-                        alt="tulsi-logo"
-                    />
-                </Link>
+        <div className='container pt-3 inset-x-0  max-w-2xl  mx-auto z-[60]'>
+            <div className="relative rounded-full border-2 border-#f0f0f0 bg-gradient-to-r from-slate-300 to-slate-500 backdrop-blur-sm flex justify-between space-x-4 px-10 py-2">
+                <div className="nleft">
+                    <Link href={"/"} >
+                        <Image
+                            src="/tulsi-logo1.png"
+                            width={100}
+                            height={100}
+                            alt="tulsi-logo"
+                        />
+                    </Link>
+                </div>
+                <div className="flex flex-row gap-6 items-center text-lg font-medium">
+                    <div className="flex gap-1 text-pretty">
+                        <Zap className="text-white" />
+                        <Link href="/services">
+                            <h1 className="cursor-pointer text-white">Services</h1>
+                        </Link>
+                    </div>
+                    <div className="flex gap-1">
+                        <Telescope className="text-white" />
+                        <Link href="/about">
+                            <h1 className="cursor-pointer text-white">About Us</h1>
+                        </Link>
+                    </div>
+                </div>
             </div>
         </div>
     );
