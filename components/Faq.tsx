25c036d29c7bab00ffa2f147c5bbb244f8599265
Abhishek <abhishekkp1721@hotmail.com>
2025-02-27 17:41:04 +0530

    Make the Faq component for accept multi line ans

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -4,23 +4,28 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion";
+import { JSX } from "react";
 
-interface FaqProps{
-    ques : string;
-    ans: string;
+interface FaqProps {
+    ques: string;
+    ans: string | JSX.Element;
 }
 
-export default function Faq({ques, ans} : FaqProps) {
+export default function Faq({ ques, ans }: FaqProps) {
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto">
             <Accordion type="single" collapsible>
                 <AccordionItem value="item-1">
                     <AccordionTrigger>{ques}</AccordionTrigger>
                     <AccordionContent>
-                        {ans}
+                        {typeof ans === "string" ? (
+                            <p>{ans}</p>
+                        ) : (
+                            ans
+                        )}
                     </AccordionContent>
                 </AccordionItem>
             </Accordion>
         </div>
-    )
+    );
 }
