d8ae250f9b2ebc9f5d058733adea2bdd5a6d24b0
Abhishek <abhishekkp1721@hotmail.com>
2025-02-27 18:05:04 +0530

    Update the wallpanel page

--- app/wallpanel/page.tsx
+++ app/wallpanel/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 // import Appbar from "@/components/Appbar";
 import ProductDescription from "@/components/ProductDescription";
 import ProductBetween from "@/components/ProductBetween";
+import Faq from "@/components/Faq";
 
 export default function Wallpanels() {
     return (
@@ -53,13 +54,40 @@ export default function Wallpanels() {
                 <div className="grid grid-cols-1 md:grid-cols-2 ">
                     <ProductDescription imageUrl={"/Wallpanels/Panel30.png"} description={"Luxury Look"} height="400px" />
                     <ProductDescription imageUrl={"/Wallpanels/Panel31.png"} description={"Wallpanel"} height="400px" />
-                    <ProductDescription imageUrl={"/Wallpanels/Panel13.jpg"} description={"Wallpanel"} height="400px"/>
-                    <ProductDescription imageUrl={"/Wallpanels/Panel26.jpg"} description={"Wallpanel"} height="400px"/>
+                    <ProductDescription imageUrl={"/Wallpanels/Panel13.jpg"} description={"Wallpanel"} height="400px" />
+                    <ProductDescription imageUrl={"/Wallpanels/Panel26.jpg"} description={"Wallpanel"} height="400px" />
                 </div>
+            </div>
 
+            {/*Frequently ask questions start from here */}
+
+            <div className="container relative max-w-screen-xl mx-auto mt-10">
+                <h1 className="text-xl font-semibold">Frequently asked questions</h1>
+                <div className="flex flex-col">
+                    <Faq
+                        ques={"Can panels be installed over existing plywood or tiles?"}
+                        ans={"Yes, they can be installed over any surface including tiles, plywood, and drywall"}
+                    />
+                    <Faq
+                        ques="How do I check if my walls are suitable for wall panels?"
+                        ans={
+                            <>
+                                All walls are suitable for panel installation.
+                                <br />
+                                Any kind of imperfections will be handled by our professional.
+                            </>
+                        }
+                    />
+                    <Faq
+                        ques={"What are PVC panels?"}
+                        ans={"PVC stands for polyvinyl chloride. It is a type of eco-friendly plastic material that is widely used today due to its affordability and ease of installation."}
+                    />
+                    <Faq
+                        ques={"What are charcoal panels?"}
+                        ans={"Charcoal wall panels are decorative panels made from charcoal and other materials, such as polystyrene or wood.They can be used for a variety of purposes"}
+                    />
+                </div>
             </div>
         </>
-
-
     );
 }
