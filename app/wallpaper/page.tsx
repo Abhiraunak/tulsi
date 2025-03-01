b748d0aa0650786e000eb7d386e17772467ea07e
Abhishek <abhishekkp1721@hotmail.com>
2025-03-01 17:45:03 +0530

    Add images for flooring

--- app/wallpaper/page.tsx
+++ app/wallpaper/page.tsx
@@ -1,7 +1,24 @@
-export default function Wallpaper(){
+import ProductAppbar from "@/components/ProductAppbar";
+import Image from "next/image";
+
+export default function Wallpaper() {
     return (
-        <div>
-            hi from wallpaper page
+        <div className="min-h-screen bg-gray-50">
+            {/* Appbar */}
+            <nav className="sticky top-0 z-50 bg-white shadow-md">
+                <ProductAppbar heading="Wallpaper" />
+            </nav>
+
+            {/* Content Section */}
+            <div className="flex justify-center items-center h-[80vh] bg-black">
+                <Image
+                    src="/tulsi comming soon.png"
+                    alt="Tulsi Coming Soon"
+                    height={300}
+                    width={300}
+                    className="object-contain"
+                />
+            </div>
         </div>
-    )
+    );
 }
