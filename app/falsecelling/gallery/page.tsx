09f932dfd67e8cb2dbd3626c811b22c6d7c9c5b2
Abhishek <abhishekkp1721@hotmail.com>
2025-03-06 15:40:56 +0530

    Add gallery page for falsecelling

--- app/falsecelling/gallery/page.tsx
+++ app/falsecelling/gallery/page.tsx
@@ -1,21 +1,48 @@
+import BookingButton from "@/components/BookingButoon";
 import ProductAppbar from "@/components/ProductAppbar";
-import Image from "next/image";
+import ProductDescription from "@/components/ProductDescription";
+import WhatsappButton from "@/components/WhatsAppIntegration";
 
 export default function Page() {
-  return (
-    <>
-      <ProductAppbar heading={"Wall panel"} />
-      <div className="flex flex-col items-center justify-center min-h-screen text-center">
-        <h1 className="text-6xl text-green-700 font-extrabold">Hi from gallery</h1>
-        <h3 className="text-xl">Stay tuned we will update soon.</h3>
-        <Image
-          src={"/tulsi-logo1.png"}
-          alt={"tulsi-logo"}
-          width={300}
-          height={300}
-          className="mt-4"
-        />
-      </div>
-    </>
-  )
+    return (
+        <div>
+            <nav className="w-full sticky top-0 z-[1000] bg-white shadow-md">
+                <ProductAppbar heading="False-Celling" />
+            </nav>
+            
+            {/* Bedroom display start from here */}
+            <div className="container relative max-w-screen-xl mx-auto pt-20">
+                <h1 className="text-3xl text-green-950 font-medium mx-3 md:mx-0">Bedroom</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    <ProductDescription imageUrl={"/Falsecelling/bedroom/design1.jpg"} description={"Bedroom"} />
+                    <ProductDescription imageUrl={"/Falsecelling/bedroom/design2.jpg"} description={"Bedroom"} />
+                    <ProductDescription imageUrl={"/Falsecelling/bedroom/design3.jpg"} description={"Bedroom"} />
+                    <ProductDescription imageUrl={"/Falsecelling/bedroom/design4.jpg"} description={"Bedroom"} />
+                    <ProductDescription imageUrl={"/Falsecelling/bedroom/design5.jpg"} description={"Bedroom"} />
+                    <ProductDescription imageUrl={"/Falsecelling/bedroom/design6.jpg"} description={"Bedroom"} />
+                </div>
+            </div>
+
+             {/* Living room display start from here */}
+             <div className="container relative max-w-screen-xl mx-auto pt-5 pb-20">
+                <h1 className="text-3xl text-green-950 font-medium mx-3 md:mx-0">Living Room</h1>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    <ProductDescription imageUrl={"/Falsecelling/livingroom/design1.jpg"} description={"Living Room"} />
+                    <ProductDescription imageUrl={"/Falsecelling/livingroom/design2.jpg"} description={"Living Room"} />
+                    <ProductDescription imageUrl={"/Falsecelling/livingroom/design3.jpg"} description={"Living Room"} />
+                    <ProductDescription imageUrl={"/Falsecelling/livingroom/design4.jpg"} description={"Living Room"} />
+                    <ProductDescription imageUrl={"/Falsecelling/livingroom/design5.jpg"} description={"Living Room"} />
+                    <ProductDescription imageUrl={"/Falsecelling/livingroom/design6.jpg"} description={"Living Room"} />
+                </div>
+            </div>
+
+            <div className="fixed bottom-0 left-0 w-full bg-gray-100 shadow-md text-white text-center p-4 z-[1000]">
+                <BookingButton />
+            </div>
+
+            <div className="fixed bottom-20 right-5 z-[1100]">
+                <WhatsappButton />
+            </div>
+        </div>
+    )
 }
