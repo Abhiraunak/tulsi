ad15a085d6ce441dde759c2b88d385e04593a9c6
Abhishek <abhishekkp1721@hotmail.com>
2025-03-07 14:07:50 +0530

    Adding backend to the form

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -7,14 +7,48 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import WhatsappButton from "@/components/WhatsAppIntegration";
+import { useState } from "react";
 
 export default function Services() {
+    const [formData, setFormData] = useState({
+        name: "",
+        phoneNumber: "",
+        service: "",
+        adress: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleServiceChange = (value: string) => {
+        setFormData({ ...formData, service: value });
+    }
+   
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const res = await fetch('/api/booking', {
+            method: "POST",
+            body: JSON.stringify({
+                formData
+            })
+        })
+
+        if(res.ok){
+            alert("Booking sucessfull !!")
+        } else {
+            alert("Booking failed !")
+        }
+    }
+
+
     return (
         <>
             <ProductAppbar heading={"Book a Service"} />
             <WhatsappButton />
             <div className="bg-slate-800 h-[105vh] md:h-screen md:w-full w-[105%]">
-                <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] px-2 md:p-6">
+                <div className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] px-2 mx-3 md:mx-0 md:p-6">
                     <BackgroundGradient>
                         <Card className="w-[25rem] rounded-2xl">
                             <CardContent>
@@ -23,12 +57,15 @@ export default function Services() {
                                         Book a service
                                     </h1>
                                 </div>
-                                <form className="space-y-3">
+                                <form onSubmit={handleSubmit} className="space-y-3">
                                     <div>
                                         <Label htmlFor="Name" className="font-semibold tracking-tight text-lg">Name</Label>
                                         <Input
                                             type="text"
                                             placeholder="Enter your Name"
+                                            value={formData.name}
+                                            onChange={handleChange}
+                                            required
                                         />
                                     </div>
                                     <div>
@@ -36,11 +73,14 @@ export default function Services() {
                                         <Input
                                             type="text"
                                             placeholder="Phone Number"
+                                            value={formData.phoneNumber}
+                                            onChange={handleChange}
+                                            required
                                         />
                                     </div>
                                     <div className="container relative">
                                         <Label htmlFor="services" className="font-semibold tracking-tight text-lg">Services</Label>
-                                        <Select>
+                                        <Select onValueChange={handleServiceChange}>
                                             <SelectTrigger className="w-full">
                                                 <SelectValue placeholder="Choose a Service" />
                                             </SelectTrigger>
@@ -58,10 +98,13 @@ export default function Services() {
                                         <Input
                                             type="text"
                                             placeholder="Address"
+                                            value={formData.adress}
+                                            onChange={handleChange}
+                                            required
                                         />
                                     </div>
                                     <div className="pt-5">
-                                        <Button className="w-full">
+                                        <Button type="submit" className="w-full">
                                             Book your service
                                         </Button>
                                     </div>
